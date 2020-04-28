ca451495c0590d6a4aa869705ba45acbf0b8b9cd
Hakkel Tamás <hakkelt@gmail.com>
2020-04-28 11:35:08 +0200

    fix test

--- test/show.jl
+++ test/show.jl
@@ -7,6 +7,6 @@ using FunctionOperators, Test
     @test sprint(show, Op₁ * Op₁) == "FunctionOperatorComposite{Float64}(Op₁ * Op₁, (10, 10), (10, 10), no plan)"
     combined = Op₁ * Op₁
     combined * ones(10, 10)
-    @test sprint(show, combined) == "FunctionOperatorComposite{Float64}(Op₁ * Op₁, (10, 10), (10, 10), Op₁.forw(Op₁.forw(x)))"
+    @test sprint(show, combined) == "FunctionOperatorComposite{Float64}(Op₁ * Op₁, (10, 10), (10, 10), buffer1 .= Op₁.forw(buffer1 .= Op₁.forw(x)))"
     @test sprint(show, "text/plain", Op₁ * Op₁, context = :module=>@__MODULE__) == "FunctionOperatorComposite with eltype Float64\n    Name: Op₁ * Op₁\n    Input dimensions: (10, 10)\n    Output dimensions: (10, 10)\n    Plan: no plan"
 end
